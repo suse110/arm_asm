d68162bc0632bd3eae01625fd7679a04bdf9e6fc
ophelia <angelina0605@foxmail.com>
2021-07-31 23:55:22 +0800

    chang debug.bat

--- first/debug.bat
+++ first/debug.bat
@@ -1,3 +1,8 @@
-start qemu-system-arm -nographic -smp 1 -machine lm3s811evb -kernel ./build/test.elf -s -S
-sleep 1
+set QEMU_ROOT=D:\workspace\tools\qemu
+SET HOME=%USERPROFILE%
+@REM set GDB_ROOT=D:\workspace\tools\gcc-arm-none-eabi-10-2020-q4-major-win32\bin
+set GDB_ROOT=C:\Users\Ophelia\.vscode\extensions\metalcode-eu.windows-arm-none-eabi-0.1.6\bin\
+set PATH=%PATH%;%QEMU_ROOT%;%GDB_ROOT%
+cmd \k qemu-system-arm -nographic -smp 1 -machine lm3s811evb -kernel ./build/test.elf -s -S
+pause
 arm-none-eabi-gdb ./build/test.elf -q -x ../gdbinit
