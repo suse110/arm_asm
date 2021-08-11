a8868376e03b210192ee755bdd2527d56a3df5c3
ophelia <angelina0605@foxmail.com>
2021-08-12 00:09:00 +0800

    add setenv.bat

---
+++ setenv.bat
@@ -0,0 +1,6 @@
+@set QEMU_ROOT=%CD%\tools\qemu\win\w64-202107062
+@set GDB_ROOT=%CD%\tools\arm-none-eabi\Sourcery_CodeBench_Lite_for_ARM_EABI\bin
+@set OPENOCD_ROOT=%CD%\tools\openocd\OpenOCD-20210519-0.11.0\bin
+@set HEXDUMP_ROOT=%CD%\tools\hexdump
+@set TOOL_ROOT=%CD%\tools\
+@set PATH=%PATH%;%QEMU_ROOT%;%GDB_ROOT%;%OPENOCD_ROOT%;%HEXDUMP_ROOT%;%TOOL_ROOT%
