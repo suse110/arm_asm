6364c84b64c46247f9df391a2ea9d7c935359ff5
ophelia <angelina0605@foxmail.com>
2021-08-14 11:18:52 +0800

    replace lite_printf with printf

--- first/app/test.c
+++ first/app/test.c
@@ -20,10 +20,10 @@ void main(void)
     stack_end_addr = (uint32_t)&_estack;
     stack_size = stack_end_addr - stack_start_addr;
 
-    lite_printf("[%s] hello world\n", __func__);
-    lite_printf("[%s] code_start_addr=0x%x code_end_addr=0x%x code_size=%d<0x%x>\n", \
+    printf("[%s] hello world\n", __func__);
+    printf("[%s] code_start_addr=0x%x code_end_addr=0x%x code_size=%d<0x%x>\n", \
         __func__, code_start_addr, code_end_addr, code_size, code_size);
-    lite_printf("[%s] stack_start_addr=0x%x stack_end_addr=0x%x stack_size=%d<0x%x>\n", \
+    printf("[%s] stack_start_addr=0x%x stack_end_addr=0x%x stack_size=%d<0x%x>\n", \
         __func__, stack_start_addr, stack_end_addr, stack_size, stack_size);
 
     // backtrace_test();
@@ -33,55 +33,55 @@ void main(void)
 
 void nmi_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void hard_fault_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void mem_manage_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void bus_fault_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void usage_fault_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void svc_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void debug_mon_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void pend_sv_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
 void sys_tick_handler(void)
 {
-    lite_printf("[%s]\n", __func__);
+    printf("[%s]\n", __func__);
     while(1);
 }
 
