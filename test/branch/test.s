e3c0f613928bd17b21b4b356ffd6aa6bb54a6dce
xuemin <suse110@foxmail.com>
2021-09-12 15:07:51 +0800

    add branch test code

--- test/branch/test.s
+++ test/branch/test.s
@@ -6,7 +6,7 @@
     BX <Rm>     Branch and exchange. Branch to an address value stored in Rm, and set the execution 
                 state of the processor (T-bit) based on bit 0 of Rm (bit 0 of Rm must be 1 because 
                 Cortex-M processor only supports Thumb state).
-    BL <label>  Branch to a labeled address and save the return address in LR
+    BL <label>  Branch to a labeled address and save the return address(PC+4) in LR
     BLX <Rm>    Branch to an address specified by Rm, save the return address in LR, and update 
                 T-bit in EPSR with LSB of Rm
 
@@ -57,8 +57,78 @@
 
 .global _start
 _start:
+
+bl if_else_branch
+bl branch_in_loop
+bl conditional_branch
+
 ldr r0, =c_entry
 bx r0
+
+
+/*if else 分支
+类似的C伪代码是这样的:
+
+int main() {
+   int max = 0;
+   int a = 2;
+   int b = 3;
+   if(a < b) {
+    max = b;
+   }
+   else {
+    max = a;
+   }
+   return max;
+}
+*/
+if_else_branch:
+    mov     r1, #2     /* 初始化 a */
+    mov     r2, #3     /* 初始化 b */
+    cmp     r1, r2     /* 比较谁更大些, 判断r1-r2的结果 */
+    blt     r1_lower   /* 如果R2更大跳转到r1_lower */
+    mov     r0, r1     /* 如果分支跳转没有发生，将R1的值放到到R0 */
+    b       end1        /* 跳转到结束 */
+r1_lower:
+    mov r0, r2         /* 将R2的值放到R0 */
+    b end1              /* 跳转到结束 */
+end1:
+    bx lr              /* THE END */
+
+/*
+循环中的条件分支,对应的C伪代码长这样子:
+int main() {
+   int a = 0;
+   while(a < 4) {
+   a= a+1;
+   }
+   return a;
+}
+*/
+branch_in_loop:
+        mov     r0, #0     /* 初始化 a */
+loop:
+        cmp     r0, #4     /* 检查 a==4 */
+        beq     end2        /* 如果是则结束 */
+        add     r0, r0, #1 /* 如果不是则加1 */
+        b loop             /* 重复循环 */
+end2:
+        bx lr              /* THE END */
+
+conditional_branch:
+    mov r0, #2
+    mov r1, #2
+    add r0, r0, r1
+    cmp r0, #4
+    beq func1
+    add r1, #5
+    b func2
+func1:
+    mov r1, r0
+    bx  lr
+func2:
+    mov r0, r1
+    bx  lr
 
 
 .section .rodata
