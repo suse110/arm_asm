251d65371d3e2a1ec44ed0f63244938252dfa3fb
xuemin <suse110@foxmail.com>
2021-09-12 11:54:42 +0800

    change build path

--- main/project/lm3s811/main/GCC/debug.bat
+++ main/project/lm3s811/main/GCC/debug.bat
@@ -4,12 +4,17 @@ set CUR_DIR=%CD%
 cd ..\..\..\..\..
 call setenv.bat
 cd %CUR_DIR%
+cd ..
+@REM 获取当前目录名
+pushd %1 & for %%i in (.) do set PROJ_NAME=%%~ni
+cd %CUR_DIR%
 
+set ELFFILE=%ROOT_DIR%\main\build\lm3s811\%PROJ_NAME%\%PROJ_NAME%.elf
 tasklist | findstr /i qemu-system-arm.exe > NUL
 if ErrorLevel == 1 (
     @echo "start qemu-system-arm"
-    start qemu-system-arm -nographic -smp 1 -machine lm3s811evb -kernel %ROOT_DIR%\main\build\lm3s811\test.elf -s -S
-    sleep 0.5
+    start qemu-system-arm -nographic -smp 1 -machine lm3s811evb -kernel %ELFFILE% -s -S
+    pause
 ) else (
 @echo "qemu-system-arm already run"
 )
@@ -18,6 +23,6 @@ tasklist | findstr /i qemu-system-arm.exe > NUL
 if ErrorLevel == 1 (
     @echo "qemu-system-arm start fail"
 ) else (
-    arm-none-eabi-gdb %ROOT_DIR%\main\build\lm3s811\test.elf -q -x gdbinit
+    arm-none-eabi-gdb %ELFFILE% -q -x gdbinit
 )
 
--- main/project/lm3s811/main/test.c
+++ main/project/lm3s811/main/test.c
@@ -14,61 +14,6 @@ void main(void)
     while(1);
 }
 
-void nmi_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void hard_fault_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void mem_manage_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void bus_fault_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void usage_fault_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void svc_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void debug_mon_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void pend_sv_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-void sys_tick_handler(void)
-{
-    printf("[%s]\n", __func__);
-    while(1);
-}
-
-
 #if 0
 
 mmu_setting_1:
