26e814f7a59cbbd1986bce636f5be971989cff77
xuemin <suse110@foxmail.com>
2025-12-22 23:45:25 +0800

    add memfault

--- main/kernel/module.mk
+++ main/kernel/module.mk
@@ -5,3 +5,7 @@ include $(SDKPATH)/$(KERNEL_DIR)/service/module.mk
 ifeq ($(FREERTOS_ENABLE),y)
 include $(SDKPATH)/$(KERNEL_DIR)/rtos/freertos/FreeRTOS/module.mk
 endif
+
+ifeq ($(MEMFAULT_ENABLE),y)
+include $(SDKPATH)/$(KERNEL_DIR)/third/memfault/module.mk
+endif
